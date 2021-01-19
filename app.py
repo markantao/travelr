8586f76bfa3003d4fff44f7e74c09251c591dc00
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-01-18 21:01:47 -0500

    added bootstrap to travlr

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("home.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
